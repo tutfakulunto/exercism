ba65ba1e2b1d3d3b25a6d2aac6f504c37de673ae
Scott Johnson <c.scott.johnson@gmail.com>
2018-01-22 10:38:09 -0600

    finished python isogram

--- python/isogram/isogram.py
+++ python/isogram/isogram.py
@@ -1,2 +1,4 @@
 def is_isogram(string):
-    pass
+    string = string.lower()
+    letter_count = [string.count(letter) for letter in string if letter.isalpha()]
+    return sum(letter_count) == len(letter_count)
